72b5cdaaeacf44de18a945f5c418cf20b3cbcf39
turnagza <turnagza@ad.unc.edu>
2023-03-23 21:42:57 -0400

    cmon mannnnnnnnnnnnn

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ app.get("/app/rpsls/play/:shot", (req, res) => {
 app.get("*", (req, res) => {
     res.status(404).send("404 NOT FOUND");});
 app.listen(PORT, () => {
-    console.log(`The app is lenstening on port ${PORT}`);});
+    console.log(`the app is lisning on port ${PORT}`);});
